388a0f03b4ba7c8a6b963598c54646602b787ff9
Ayushi150104 <choudharybinaykumar0@gmail.com>
2026-08-22 16:30:19 +0530

    Production ready enviorment

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    "https://juice-commercial-project-user-ready-to-n2gk.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
