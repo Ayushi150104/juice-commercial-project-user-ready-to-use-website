e356d840905dbfcbe10c658a28574427787c12f5
Ayushi150104 <choudharybinaykumar0@gmail.com>
2026-08-22 16:47:28 +0530

    Production ready enviorment

--- src/component/SidebarFr.jsx
+++ src/component/SidebarFr.jsx
@@ -223,6 +223,7 @@ function SidebarFr(props) {
                 <button
                   onClick={() => {
                     localStorage.removeItem("user");
+                    localStorage.setItem("token", "");
                     window.location.reload();
                   }}
                   className="text-red-400 text-sm mt-2 hover:text-red-500"
